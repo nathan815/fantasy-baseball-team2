a0ee05a88c59d55e3dc652e7461bf618ae4b9a41
Will Shields <43360999+wshields@users.noreply.github.com>
2019-11-20 16:11:23 -0500

    Barebones setup of Player, Pitcher, Hitter classes

--- src/Hitter.java
+++ src/Hitter.java
@@ -1,2 +1,6 @@
 public class Hitter extends Player {
+
+    public Hitter(String name, String team){
+        super(name, team);
+    }
 }
--- src/Pitcher.java
+++ src/Pitcher.java
@@ -1,2 +1,6 @@
 public class Pitcher extends Player {
+
+    public Pitcher(String name, String team){
+        super(name, team);
+    }
 }
