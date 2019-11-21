945d3ebce2e9b283fa728a895b4c587938bed53e
EbrahimQasem <eaaqasem@gmail.com>
2019-11-20 20:26:15 -0500

    Updated Driver Class

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,40 @@
+import java.util.*;
+
 public class Driver {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter your order: ");
+        String user = input.nextLine();
+
+        String[] array = userSplit(user);
+
+        for(int i=0; i<array.length; i++)
+            System.out.println(array[i]);
+
+        String request = order(array[0]);
+        System.out.println(request);
+    }
+
+    public static String[] userSplit(String str) {
+        String[] userArray = str.split("\\s+");
+        return userArray;
+    }
+
+    public static String order(String userInput) {
+        String str = " ";
+        String[] orders = {"ODRAFT", "IDRAFT", "OVERALL",
+                "POVERALL", "TEAM", "STARS",
+                "SAVE","QUIT", "RESTORE",
+                "EVALFUN", "PEVALFUN"};
+
+        for(int i=0; i<orders.length; i++) {
+
+            if(userInput.equals(orders[i]))
+                return str = orders[i];
+            else
+                str = "Invalid Input";
+        }
+        return str;
     }
 }
