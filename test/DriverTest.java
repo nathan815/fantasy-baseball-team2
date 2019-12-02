08c032e9f7fe03f0367a5f3d226fb3c170d61329
EbrahimQasem <eaaqasem@gmail.com>
2019-12-01 19:58:27 -0500

    Issue #10
    Added JUnit to the update userSplit Method()

--- test/DriverTest.java
+++ test/DriverTest.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 
 public class DriverTest {
 
-    @Test
-    public void testSplitFunction() {
-        String str = "IDRAFT ODRAFT OVERALL";
-        String[] arr = {"IDRAFT", "ODRAFT", "OVERALL"};
-        assertEquals(arr, Driver.userSplit(str));
-    }
+//    @Test
+//    public void testSplitFunction() {
+//        String str = "IDRAFT ODRAFT OVERALL";
+//        String[] arr = {"IDRAFT", "ODRAFT", "OVERALL"};
+//        assertEquals(arr, Driver.userSplit(str));
+//    }
 
     @Test
     public void testValidInput() {
@@ -22,4 +22,26 @@ public class DriverTest {
         String str = "ABCDE";
         assertEquals("", Driver.order(str));
     }
+
+    @Test
+    public void testUserSplitODRAFT(){
+        String[] arr = {"ODRAFT", "Cruz, N", "A"};
+        assertArrayEquals(arr, Driver.userSplit("ODRAFT \"Cruz, N\" A"));
+    }
+
+    @Test
+    public void testUserSplitIDRAFT(){
+        String[] arr = {"IDRAFT", "Gray"};
+        assertArrayEquals(arr, Driver.userSplit("IDRAFT \"Gray\""));
+    }
+
+    @Test
+    public void testUserSplitSaveRestore(){
+        String[] arr = {"SAVE", "book"};
+        assertArrayEquals(arr, Driver.userSplit("SAVE book"));
+        String[] arr1 = {"RESTORE", "book1"};
+        assertArrayEquals(arr1, Driver.userSplit("RESTORE book1"));
+
+    }
+
 }
